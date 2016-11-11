3be3338f9bae9e43ccb925339184e57f14f959e2
Zack Winchell <zackwinchell@gmail.com>
2016-11-10 17:22:12 -0800

    Fix Stormpath application not being defined

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,24 @@
 var express = require('express');
 var router = express.Router();
 var User = require('../models/user');
-// var firebase = require("firebase");
+var stormpath = require("stormpath");
+
+/* Stormpath setup */
+
+var apiKey = new stormpath.ApiKey(
+  process.env['STORMPATH_CLIENT_APIKEY_ID'],
+  process.env['STORMPATH_CLIENT_APIKEY_SECRET']
+);
+
+var client = new stormpath.Client({ apiKey: apiKey });
+var applicationHref = process.env['STORMPATH_APPLICATION_HREF'];
+var application;
+client.getApplication(applicationHref, function(err, stormApp) {
+  console.log('Application:', stormApp);
+  application = stormApp;
+});
+
+/* Routes */
 
 router.post('/register', function(req, res, next) {
   console.log('in register');
@@ -33,9 +50,14 @@ view link to see how to set up application
 https://api.stormpath.com/ui2/index.html#/quickstart/none/nodejs/backend/project-type/existing
 */
 application.createAccount(account, function(err, createdAccount) {
-  console.log('Account:', createdAccount);
+  if (err) {
+    console.log('Error when creating account', account);
+    res.status(400).send(err.userMessage);
+  } else {
+    console.log('Account:', createdAccount);
+    res.send();
+  }
 });
-res.send();
 
 });
 
